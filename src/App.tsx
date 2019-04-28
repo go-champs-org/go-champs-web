854eae7050ae9341c077cc87f45e1720745319fa
Lair Júnior <lairjr@outlook.com>
2019-04-28 15:48:09 -0400

    Add router link and make app navigatable

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import './App.scss';
-import Home from './Tournament/Home';
+import { default as TournamentHome } from './Tournament/Home';
+import { default as UserHome } from './User/Home';
+
+
+const Index: React.FC = () => {
+  return (
+    <div>
+      <h1>Index</h1>
+      <Link to="/secretaria-esportes-poa">Secretaria Municipal de Esportes</Link>
+    </div>
+  );
+}
 
 const App: React.FC = () => {
   return (
-    <section className="section">
-      <div className="container">
-        <Home />
-      </div>
-    </section>
+    <Router>
+      <nav>Nav</nav>
+      <section className="section">
+        <div className="container">
+          <Route exact path="/" component={Index} />
+          <Route exact path={`/:userId`} component={UserHome} />
+          <Route exact path={`/:userId/:tournamentId`} component={TournamentHome} />
+        </div>
+      </section>
+    </Router>
   );
 }
 
--- src/User/Home.tsx
+++ src/User/Home.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Link } from "react-router-dom";
 import { mockUserData, TournamentData } from './data';
 
-const TournamentCard: React.FC<{ tournament: TournamentData }> = ({ tournament }) => {
+const TournamentCard: React.FC<{ tournament: TournamentData, url: string }> = ({ tournament, url }) => {
     return (
-        <div className="card">
-            <header className="card-header">
-                <p className="card-title">{tournament.name}</p>
-            </header>
-        </div>
+        <Link to={`${url}/${tournament.link}`}>
+            <div className="card">
+                <header className="card-header">
+                    <p className="card-title">{tournament.name}</p>
+                </header>
+            </div>
+        </Link>
     );
 }
 
-const Home: React.FC = () => {
+interface MatchParams {
+    tournamentId: string;
+}
+
+const Home: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
     const user = mockUserData;
+    console.log('user home', match.params.tournamentId)
     return (
         <div className="columns is-multiline">
             <div className="column is-12">
@@ -21,7 +30,7 @@ const Home: React.FC = () => {
                 </a>
             </div>
             <div className="column is-12">
-                {user.tournaments.map((tournament: TournamentData) => <TournamentCard tournament={tournament} />)}
+                {user.tournaments.map((tournament: TournamentData) => <TournamentCard tournament={tournament} url={match.url} />)}
             </div>
         </div>
     );
