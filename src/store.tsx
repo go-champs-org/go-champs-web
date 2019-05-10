74b8a4b33c2a34e572e1cd421694c1bc2bddd969
Lair Júnior <lairjr@outlook.com>
2019-05-10 01:11:34 -0400

    Setup redux dev tools

--- src/store.tsx
+++ src/store.tsx
@@ -1,4 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import { default as organizationReducer } from './Organizations/reducer';
 
@@ -6,7 +7,10 @@ const simpleReducer = (state = {}, action = {}) => {
     return state;
 }
 
-export default createStore(combineReducers({
-    simpleReducer,
-    organizationReducer,
-}), applyMiddleware(thunkMiddleware));
+export default createStore(
+    combineReducers({
+        simpleReducer,
+        organizationReducer,
+    }),
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+);
