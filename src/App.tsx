b8c89852676b634cb4043a99af1ffef5bbe08753
Lair Júnior <lairjr@outlook.com>
2019-05-12 01:16:51 -0400

    Clean up project

--- src/App.tsx
+++ src/App.tsx
@@ -3,74 +3,9 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import Home from './Pages/Home';
+import NavBar from './Shared/NavBar';
 import store from './store';
-import { default as TournamentHome } from './Tournament/Home';
-import { default as UserHome } from './User/Home';
 
-const NavBar: React.FC = () => {
-  return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-        </a>
-
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">
-            Home
-      </a>
-
-          <a className="navbar-item">
-            Documentation
-      </a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">
-              More
-        </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item">
-                About
-          </a>
-              <a className="navbar-item">
-                Jobs
-          </a>
-              <a className="navbar-item">
-                Contact
-          </a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item">
-                Report an issue
-                </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">
-                Log in
-          </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-}
 
 const App: React.FC = () => {
   return (
@@ -80,8 +15,6 @@ const App: React.FC = () => {
         <section className="section">
           <div className="container">
             <Route exact path="/" component={Home} />
-            <Route exact path={`/:userId`} component={UserHome} />
-            <Route exact path={`/:userId/:tournamentId`} component={TournamentHome} />
           </div>
         </section>
       </Router>
