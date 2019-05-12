ac168ca2f1e2293dc14daa2b0d55f5b8fdcfbc9f
Lair Júnior <lairjr@outlook.com>
2019-05-12 02:00:15 -0400

    List tournaments on organization home page

--- src/Pages/OrganizationHome.tsx
+++ src/Pages/OrganizationHome.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { bindActionCreators } from 'redux';
+import { requestTournaments } from '../Tournaments/actions';
+import { List } from '../Tournaments/List';
+import { TournamentState } from '../Tournaments/state';
 
-class OrganizationHome extends React.Component {
+interface OrganizationHomeProps extends RouteComponentProps {
+    tournamentState: TournamentState,
+    requestTournaments: any,
+}
+
+class OrganizationHome extends React.Component<OrganizationHomeProps> {
     render() {
         return (
             <div>
-                Organization Home
+                <List tournamentState={this.props.tournamentState} url={this.props.match.url} />
             </div>
         )
     }
+
+    componentDidMount() {
+        this.props.requestTournaments();
+    }
 }
 
-export default OrganizationHome;
+const mapStateToProps = (state: any) => ({
+    tournamentState: state.tournaments,
+});
+
+const mapDispatchToProps = (dispatch: any) => (
+    bindActionCreators({
+        requestTournaments,
+    }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrganizationHome);
--- src/store.tsx
+++ src/store.tsx
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { default as organizationReducer } from './Organizations/reducer';
 import { fetchMiddleware } from './Shared/store/middlewares';
+import { default as tournamentReducer } from './Tournaments/reducer';
 
 export default createStore(
     combineReducers({
         organizations: organizationReducer,
+        tournaments: tournamentReducer,
     }),
     composeWithDevTools(
         applyMiddleware(fetchMiddleware),
