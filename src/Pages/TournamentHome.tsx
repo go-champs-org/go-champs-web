83591615b7ee72179e08cca888969de0a9c04d44
Lair Júnior <lairjr@outlook.com>
2019-05-12 03:11:23 -0400

    Use slug to display tournament name

--- src/Pages/TournamentHome.tsx
+++ src/Pages/TournamentHome.tsx
@@ -5,7 +5,12 @@ import { bindActionCreators } from "redux";
 import { requestTournament } from "../Tournaments/actions";
 import { TournamentState } from "../Tournaments/state";
 
-interface TournamentHomeProps extends RouteComponentProps {
+interface MatchProps {
+    tournamentId: string,
+    organizationId: string,
+}
+
+interface TournamentHomeProps extends RouteComponentProps<MatchProps> {
     tournamentState: TournamentState,
     requestTournament: any,
 }
@@ -16,13 +21,14 @@ class TournamentHome extends React.Component<TournamentHomeProps> {
             <div>
                 {this.props.tournamentState.isLoadingRequestTournament ?
                     <div>Loading...</div> :
-                    <div>{this.props.tournamentState.tournaments["a0f1ffed-8e1c-46f2-8179-41085d10401d"] && this.props.tournamentState.tournaments["a0f1ffed-8e1c-46f2-8179-41085d10401d"].name}</div>
+                    <div>{this.props.tournamentState.tournaments[this.props.match.params.tournamentId] && this.props.tournamentState.tournaments[this.props.match.params.tournamentId].name}</div>
                 }
             </div>
         )
     }
 
     componentDidMount() {
+        // TODO: Allow request with slug
         this.props.requestTournament("a0f1ffed-8e1c-46f2-8179-41085d10401d");
     }
 }
