d1f223fa92b34bbad796962dd3383cab5997ae05
Lair Júnior <lairjr@outlook.com>
2019-05-31 00:31:34 -0400

    Rename unnecessary tsx to ts files

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Router>
-        {/* <NavBar /> */}
         <section className="section">
           <div className="container">
             <Switch>
