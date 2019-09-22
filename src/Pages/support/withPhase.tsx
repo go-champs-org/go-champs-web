84338e06dc2b918c7f3fec6498d7c926272a8fa0
Lair Júnior <lairjr@outlook.com>
2019-09-22 00:10:41 -0400

    Load phase games in with phase

--- src/Pages/support/withPhase.tsx
+++ src/Pages/support/withPhase.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { bindActionCreators, Dispatch } from 'redux';
 import { StoreState } from '../../store';
+import { requestTournamentGames } from '../../Tournaments/Games/actions';
 import { requestTournamentPhase } from '../../Tournaments/Phases/actions';
 import { tournamentBySlug } from '../../Tournaments/selectors';
 import { TournamentEntity } from '../../Tournaments/state';
@@ -11,6 +12,7 @@ import { TournamentPhaseHomeMatchProps } from './routerInterfaces';
 interface WithPhaseProps
   extends RouteComponentProps<TournamentPhaseHomeMatchProps> {
   requestTournamentPhase: (tournamentId: string, phaseId: string) => {};
+  requestTournamentGames: (phaseId: string) => {};
   tournament: TournamentEntity;
 }
 
@@ -28,6 +30,7 @@ const withPhase = (WrappedComponent: any) => {
         }
       } = this.props;
       this.props.requestTournamentPhase(tournament.id, phaseId);
+      this.props.requestTournamentGames(phaseId);
     }
   }
 
@@ -45,6 +48,7 @@ const withPhase = (WrappedComponent: any) => {
   const mapDispatchToProps = (dispatch: Dispatch) => {
     return bindActionCreators(
       {
+        requestTournamentGames,
         requestTournamentPhase
       },
       dispatch
