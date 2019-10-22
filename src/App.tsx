415720939cf186e2b1e287880391b2494645765e
Lair Júnior <lairjr@outlook.com>
2019-10-22 01:58:49 -0400

    Add basic home

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,29 @@ const App: React.FC = () => {
               </Switch>
             </div>
           </section>
+
+          <footer className="footer">
+            <div className="content has-text-centered">
+              <p>
+                <strong>Go Champs!</strong> by Lair Júnior. The source code is
+                licensed&nbsp;
+                <a
+                  href="http://opensource.org/licenses/mit-license.php"
+                  style={{ color: '#970c10' }}
+                >
+                  MIT
+                </a>
+                . The website content is licensed&nbsp;
+                <a
+                  href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+                  style={{ color: '#970c10' }}
+                >
+                  CC BY NC SA 4.0
+                </a>
+                .
+              </p>
+            </div>
+          </footer>
         </DndProvider>
       </Router>
     </Provider>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import {
   deleteOrganization,
   getOrganizations,
   postOrganization
 } from '../Organizations/effects';
-import { List } from '../Organizations/List';
 import { OrganizationState } from '../Organizations/state';
 import { StoreState } from '../store';
 
@@ -22,13 +20,51 @@ interface HomeProps extends RouteComponentProps {
 class Home extends React.Component<HomeProps> {
   render() {
     return (
-      <div>
-        <Link to="/New">New</Link>
-        <List
-          organizationState={this.props.organizationState}
-          url={this.props.match.url}
-          deleteOrganization={this.props.deleteOrganization}
-        />
+      <div className="hero is-medium">
+        <div className="hero-head">
+          <div className="content">
+            <div className="columns is-vcentered">
+              <div className="column is-6">
+                <h1 className="title">Go Champs!</h1>
+
+                <span className="subtitle">
+                  A web app mais maneira para gerenciar torneios
+                </span>
+              </div>
+
+              <div className="column is-6">
+                <img src="https://cronhub.io/img/ch-dashboard-shadow.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="hero-body">
+          <div className="has-text-centered">
+            <h1 className="title">O que é o Go Champs?</h1>
+
+            <p style={{ marginBottom: '1.5rem' }}>
+              Go Champs! É uma aplicacão para você criar seu torneio e gerenciar
+              fases, times, jogos, classificacões e muito mais.
+            </p>
+
+            <p style={{ marginBottom: '1.5rem' }}>
+              As informacões ficam disponibilizadas para qualquer pessoa acessar
+              e pode acompanhar seu torneio.
+            </p>
+
+            <p style={{ marginBottom: '1.5rem' }}>
+              Acesse nosso torneio exemplo clicando&nbsp;
+              <a
+                href="sec-mun-esportes/liga-municipal"
+                style={{ color: '#970c10' }}
+              >
+                aqui
+              </a>
+              .
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
