6d3fe784fab3d8ea52b460aa3710f336b1b32a96
Lair Júnior <lairjr@outlook.com>
2020-01-28 01:40:34 -0500

    Add basic account page

--- src/Pages/AccountHome.tsx
+++ src/Pages/AccountHome.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const AccountHome: React.FC = () => {
-  return <div>Account home</div>;
+  return (
+    <div>
+      <div className="columns is-multiline">
+        <header className="column is-12">My account</header>
+
+        <div className="column is-8">Organizations</div>
+
+        <div className="column is-4">
+          <aside className="menu">
+            <p className="menu-label">General</p>
+
+            <ul className="menu-list">
+              <li>
+                <a>Organizations</a>
+              </li>
+            </ul>
+          </aside>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AccountHome;
