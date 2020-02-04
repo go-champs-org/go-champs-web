8b11de2da2362440553c1b6a675b42edff771db9
Lair Júnior <lairjr@outlook.com>
2020-02-04 01:33:08 -0500

    Adjust style on phase not found page

--- src/Pages/PhaseNotFound.tsx
+++ src/Pages/PhaseNotFound.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const PhaseNotFound: React.FC = () => <div>Create new phase</div>;
+const PhaseNotFound: React.FC = () => (
+  <div className="notification is-danger is-light">
+    <div className="content has-text-centered">
+      <h2 className="subtitle">Você precisa criar um fase para o torneio.</h2>
+
+      <button className="button is-large is-fullwidth">
+        New phase
+      </button>
+    </div>
+  </div>
+);
 
 export default PhaseNotFound;
