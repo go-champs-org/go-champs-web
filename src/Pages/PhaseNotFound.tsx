fdedf1b1148be212131fb03983d7ab47aba88c3b
Lair Júnior <lairjr@outlook.com>
2020-02-05 00:37:51 -0500

    Adjust phase not found page

--- src/Pages/PhaseNotFound.tsx
+++ src/Pages/PhaseNotFound.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
+import AdminWrapper, { NotAdminWrapper } from '../Shared/UI/AdminWrapper';
 
 const PhaseNotFound: React.FC = () => (
   <div className="column is-12">
-    <div className="notification is-danger is-light">
-      <div className="content has-text-centered">
-        <h2 className="subtitle">Você precisa criar times para o torneio.</h2>
+    <AdminWrapper>
+      <div className="notification is-danger is-light">
+        <div className="content has-text-centered">
+          <h2 className="subtitle">Você precisa criar times para o torneio.</h2>
 
-        <button className="button is-large is-fullwidth">New team</button>
+          <button className="button is-large is-fullwidth">New team</button>
+        </div>
       </div>
-    </div>
 
-    <div className="notification is-danger is-light">
-      <div className="content has-text-centered">
-        <h2 className="subtitle">Você precisa criar um fase para o torneio.</h2>
+      <div className="notification is-danger is-light">
+        <div className="content has-text-centered">
+          <h2 className="subtitle">
+            Você precisa criar um fase para o torneio.
+          </h2>
 
-        <button className="button is-large is-fullwidth">New phase</button>
+          <button className="button is-large is-fullwidth">New phase</button>
+        </div>
       </div>
-    </div>
+    </AdminWrapper>
+
+    <NotAdminWrapper>
+      <div className="notification is-danger is-light">
+        <div className="content has-text-centered">
+          <h2 className="subtitle">Este torneiro está em construção.</h2>
+        </div>
+      </div>
+    </NotAdminWrapper>
   </div>
 );
 
--- src/Shared/UI/AdminWrapper.tsx
+++ src/Shared/UI/AdminWrapper.tsx
@@ -1,5 +1,15 @@
 import React, { Fragment, ReactNode } from 'react';
 
+export const NotAdminWrapper: React.FC<{ children: ReactNode }> = ({
+  children
+}) => {
+  if (process.env.REACT_APP_APP_MODE !== 'adm') {
+    return <Fragment>{children}</Fragment>;
+  }
+
+  return <div></div>;
+};
+
 const AdminWrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
   if (process.env.REACT_APP_APP_MODE === 'adm') {
     return <Fragment>{children}</Fragment>;
