6bf3dbc6cde30b1f28d8ea019608d3181e9bb004
Lair Júnior <lairjr@outlook.com>
2020-02-14 00:05:05 -0500

    Add phase loader component

--- src/Pages/PhaseLoader.tsx
+++ src/Pages/PhaseLoader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { StoreState } from '../store';
 import { currentPhaseId } from '../Phases/selectors';
 import { tournamentLoading } from '../Tournaments/selectors';
@@ -10,6 +10,80 @@ import { getGamesByFilter } from '../Games/effects';
 import { RouteProps } from './support/routerInterfaces';
 import ComponentLoader from '../Shared/UI/ComponentLoader';
 import PhaseHome from './PhaseHome';
+import Shimmer from '../Shared/UI/Shimmer';
+
+export const PhaseHomeLoading: React.FC = () => (
+  <Fragment>
+    <div className="column is-12">
+      <Shimmer>
+        <div
+          style={{
+            height: '13px',
+            marginTop: '13px',
+            width: '250px'
+          }}
+        ></div>
+      </Shimmer>
+    </div>
+
+    <div className="column">
+      <div className="columns is-multiline">
+        <div className="column is-12">
+          <Shimmer>
+            <div
+              style={{
+                height: '150px',
+                marginTop: '13px',
+                width: '350px'
+              }}
+            ></div>
+          </Shimmer>
+        </div>
+
+        <div className="column is-12">
+          <Shimmer>
+            <div
+              style={{
+                height: '150px',
+                marginTop: '13px',
+                width: '350px'
+              }}
+            ></div>
+          </Shimmer>
+        </div>
+      </div>
+    </div>
+
+    <div className="is-divider-vertical"></div>
+
+    <aside className="column is-4">
+      <div className="columns is-multiline">
+        <div className="column is-12 has-text-centered">
+          <Shimmer>
+            <div
+              style={{
+                height: '13px',
+                marginTop: '13px',
+                width: '260px'
+              }}
+            ></div>
+          </Shimmer>
+        </div>
+        <div className="column is-12 has-text-centered">
+          <Shimmer>
+            <div
+              style={{
+                height: '150px',
+                marginTop: '13px',
+                width: '260px'
+              }}
+            ></div>
+          </Shimmer>
+        </div>
+      </div>
+    </aside>
+  </Fragment>
+);
 
 const mapStateToProps = (state: StoreState) => ({
   currentPhaseId: currentPhaseId(state.phases),
@@ -42,7 +116,7 @@ const PhaseLoader: React.FC<PhaseLoaderProps> = ({
   return (
     <ComponentLoader
       canRender={!tournamentLoading}
-      loader={<div>Loading...</div>}
+      loader={<PhaseHomeLoading />}
     >
       <PhaseHome
         organizationSlug={organizationSlug}
