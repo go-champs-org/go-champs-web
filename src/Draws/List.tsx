d1b86ce06907e6aed6ae33b48e31a59fe2283566
Lair Júnior <lairjr@outlook.com>
2020-02-21 01:23:04 -0500

    List draws on draw list page

--- src/Draws/List.tsx
+++ src/Draws/List.tsx
@@ -1,15 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Shimmer from '../Shared/UI/Shimmer';
 import { DrawEntity } from './state';
-import { Dispatch, AnyAction } from 'redux';
+import { AnyAction, Dispatch } from 'redux';
 
-export const ListLoading: React.FC = () => <div>Loading</div>;
+const LoadingCard: React.FC = () => (
+  <div className="card item">
+    <div className="card-header">
+      <div className="card-header-title">
+        <Shimmer>
+          <div
+            style={{
+              height: '13px',
+              marginTop: '13px',
+              width: '250px'
+            }}
+          ></div>
+        </Shimmer>
+      </div>
+    </div>
+  </div>
+);
 
-const List: React.FC<{
+export const ListLoading: React.FC = () => (
+  <div>
+    <LoadingCard />
+    <LoadingCard />
+    <LoadingCard />
+  </div>
+);
+
+const DrawCard: React.FC<{
+  baseUrl: string;
+  draw: DrawEntity;
+  deleteDraw: (
+    draw: DrawEntity
+  ) => (dispatch: Dispatch<AnyAction>) => Promise<void>;
+}> = ({ baseUrl, draw, deleteDraw }) => (
+  <div className="card item">
+    <div className="card-header">
+      <Link className="card-header-title" to={`${baseUrl}/EditDraw/${draw.id}`}>
+        <span className="title is-6">{draw.title}</span>
+      </Link>
+
+      <div className="card-header-icon">
+        <button className="button is-text" onClick={() => deleteDraw(draw)}>
+          <i className="fas fa-trash" />
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export const List: React.FC<{
   baseUrl: string;
   deleteDraw: (
-    game: DrawEntity
+    draw: DrawEntity
   ) => (dispatch: Dispatch<AnyAction>) => Promise<void>;
   draws: DrawEntity[];
-}> = () => <div>Draw list</div>;
+}> = ({ baseUrl, deleteDraw, draws }) => (
+  <div>
+    {draws.map((draw: DrawEntity) => (
+      <DrawCard
+        key={draw.id}
+        baseUrl={baseUrl}
+        deleteDraw={deleteDraw}
+        draw={draw}
+      />
+    ))}
+  </div>
+);
 
 export default List;
