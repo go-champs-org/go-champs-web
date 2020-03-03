da0b9742df0d0646301887ea38b52814d7e1d94c
Lair Júnior <lairjr@outlook.com>
2020-03-03 02:11:47 -0500

    Display social networks on tournament page

--- src/Tournaments/Common/TopLevel.tsx
+++ src/Tournaments/Common/TopLevel.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { TournamentEntity } from '../state';
 import Shimmer from '../../Shared/UI/Shimmer';
 import AdminWrapper from '../../Shared/UI/AdminWrapper';
+import './TopLevel.scss';
 
 export const LoadingTopLevel: React.FC = () => (
   <nav className="level">
@@ -26,32 +27,82 @@ const TopLevel: React.FC<{
   organizationSlug: string;
   tournament: TournamentEntity;
   tournamentSlug: string;
-}> = ({ organizationSlug, tournament, tournamentSlug }) => (
-  <nav className="level">
-    <div className="level-left">
-      <div className="level-item">
-        <Link to={`/${organizationSlug}/${tournamentSlug}`}>
-          <h1 className="title">{tournament.name}</h1>
-        </Link>
-      </div>
-    </div>
-
-    <AdminWrapper>
-      <div className="level-right">
+}> = ({ organizationSlug, tournament, tournamentSlug }) => {
+  const hasAnySocialNetword =
+    tournament.facebook || tournament.instagram || tournament.siteUrl;
+  return (
+    <nav className="level">
+      <div className="level-left">
         <div className="level-item">
-          <Link to={`/${organizationSlug}/${tournamentSlug}/Manage`}>
-            <button className="button is-large">
-              <span className="icon is-medium">
-                <i className="fas fa-cog"></i>
-              </span>
-
-              <span>Manage</span>
-            </button>
+          <Link to={`/${organizationSlug}/${tournamentSlug}`}>
+            <h1 className="title">{tournament.name}</h1>
           </Link>
         </div>
       </div>
-    </AdminWrapper>
-  </nav>
-);
+
+      <div className="level-right">
+        {hasAnySocialNetword && (
+          <div className="level-item">
+            {tournament.siteUrl && (
+              <a
+                href={tournament.siteUrl}
+                className="has-text-dark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span
+                  className="icon is-medium social-icon"
+                  style={{ cursor: 'pointer' }}
+                >
+                  <i className="fas fa-external-link-alt fa-lg"></i>
+                </span>
+              </a>
+            )}
+
+            {tournament.instagram && (
+              <a
+                href={tournament.instagram}
+                className="has-text-dark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon is-medium social-icon">
+                  <i className="fab fa-instagram fa-lg"></i>
+                </span>
+              </a>
+            )}
+
+            {tournament.facebook && (
+              <a
+                href={tournament.facebook}
+                className="has-text-dark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon is-medium social-icon">
+                  <i className="fab fa-facebook fa-lg"></i>
+                </span>
+              </a>
+            )}
+          </div>
+        )}
+
+        <AdminWrapper>
+          <div className="level-item">
+            <Link to={`/${organizationSlug}/${tournamentSlug}/Manage`}>
+              <button className="button is-large">
+                <span className="icon is-medium">
+                  <i className="fas fa-cog"></i>
+                </span>
+
+                <span>Manage</span>
+              </button>
+            </Link>
+          </div>
+        </AdminWrapper>
+      </div>
+    </nav>
+  );
+};
 
 export default TopLevel;
