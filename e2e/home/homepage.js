54a592f98c8dc0b3697d17807c9314f20e416fde
Lair Júnior <lairjr@outlook.com>
2020-03-06 01:01:26 -0500

    Cover send e-mail functionality

--- e2e/home/homepage.js
+++ e2e/home/homepage.js
@@ -1,9 +1,21 @@
 module.exports = {
-  'Load home page' : function (client) {
+  'Load home page': function (client) {
     client
       .url(client.launchUrl)
       .waitForElementVisible('body', 1000)
       .assert.title('Go Champs!')
-      .end()
+      .end();
+  },
+
+  'Send email': function (client) {
+    client
+      .url(client.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name=name]', 'Some name')
+      .setValue('input[name=email]', 'test@test.com')
+      .setValue('textarea[name=message]', 'Some message')
+      .click('button[type=submit]')
+      .assert.containsText('.notification', 'Mensagem enviada com sucesso!')
+      .end();
   }
 }
