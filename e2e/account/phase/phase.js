53d4c8ae64348d613055e4d5d674100195917409
Lair Júnior <lairjr@outlook.com>
2020-03-07 12:26:40 -0500

    Add coverage for CRUD phase

---
+++ e2e/account/phase/phase.js
@@ -0,0 +1,45 @@
+module.exports = {
+  'Add new phase': function (client) {
+    client
+      .url(`${client.launchUrl}/test-organization-cannot-delete/test-tournament-cannot-delete`)
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .click("//*[contains(text(), 'Manage')]")
+      .click("//*[contains(text(), 'Phases')]")
+      .assert.title('Go Champs! | Test tournament (cannot delete)')
+      .useCss()
+      .click('a[href="/test-organization-cannot-delete/test-tournament-cannot-delete/NewPhase"]')
+      .setValue('input[name="title"]', 'Test phase (can delete)')
+      .click('button[type=submit]')
+      .assert.containsText('.notification', 'Test phase (can delete) created!')
+      .end();
+  },
+
+  'Edit phase': function (client) {
+    client
+    .url(`${client.launchUrl}/test-organization-cannot-delete/test-tournament-cannot-delete`)
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .click("//*[contains(text(), 'Manage')]")
+      .click("//*[contains(text(), 'Phases')]")
+      .click("//*[contains(text(), 'Test phase (can delete)')]")
+      .useCss()
+      .setValue('input[name="title"]', ' edited')
+      .click('button[type=submit]')
+      .assert.containsText('.notification', 'Test phase (can delete) edited updated!')
+      .end();
+  },
+
+  'Delete phase': function (client) {
+    client
+      .url(`${client.launchUrl}/test-organization-cannot-delete/test-tournament-cannot-delete`)
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .click("//*[contains(text(), 'Manage')]")
+      .click("//*[contains(text(), 'Phases')]")
+      .click("//*[contains(text(), 'Test phase (can delete) edited')]/../../div/button[2]")
+      .useCss()
+      .assert.containsText('.notification', 'Test phase (can delete) edited deleted!')
+      .end();
+  }
+}
