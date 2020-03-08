5c568ffa9606b1fb4becca20d6ad3c8f11bfacaa
Lair Júnior <lairjr@outlook.com>
2020-03-08 13:46:46 -0400

    Add coverage for CRUD elimination

---
+++ e2e/account/elimination/elimination.js
@@ -0,0 +1,48 @@
+module.exports = {
+  'Add new elimination': function (client) {
+    client
+      .url(`${client.launchUrl}/test-organization-cannot-delete/test-tournament-cannot-delete`)
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .click("//*[contains(text(), 'Manage')]")
+      .click("//*[contains(text(), 'Eliminations')]")
+      .assert.title('Go Champs! | Test tournament (cannot delete)')
+      .click("//*[contains(text(), 'New')]/../a")
+      .useCss()
+      .setValue('input[name="title"]', 'Test elimination (can delete)')
+      .setValue('input[name="info"]', 'Info')
+      .click('button[type=submit]')
+      .assert.containsText('.notification', 'Test elimination (can delete) created!')
+      .end();
+  },
+
+  'Edit elimination': function (client) {
+    client
+    .url(`${client.launchUrl}/test-organization-cannot-delete/test-tournament-cannot-delete`)
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .click("//*[contains(text(), 'Manage')]")
+      .click("//*[contains(text(), 'Eliminations')]")
+      .assert.title('Go Champs! | Test tournament (cannot delete)')
+      .click("//*[contains(text(), 'Test elimination (can delete)')]")
+      .useCss()
+      .setValue('input[name="title"]', ' edited')
+      .click('button[type=submit]')
+      .assert.containsText('.notification', 'Test elimination (can delete) edited updated!')
+      .end();
+  },
+
+  'Delete elimination': function (client) {
+    client
+      .url(`${client.launchUrl}/test-organization-cannot-delete/test-tournament-cannot-delete`)
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .click("//*[contains(text(), 'Manage')]")
+      .click("//*[contains(text(), 'Eliminations')]")
+      .assert.title('Go Champs! | Test tournament (cannot delete)')
+      .click("//*[contains(text(), 'Test elimination (can delete) edited')]/../../div/button")
+      .useCss()
+      .assert.containsText('.notification', 'Test elimination (can delete) edited deleted!')
+      .end();
+  }
+}
