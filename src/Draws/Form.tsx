ec837d2c16f4d8d8ef09bd93c09f4c725a346369
Lair Júnior <lairjr@outlook.com>
2020-03-15 16:03:48 -0400

    Use collapsible card on draw form

--- src/Draws/Form.tsx
+++ src/Draws/Form.tsx
@@ -6,142 +6,158 @@ import { FieldArray } from 'react-final-form-arrays';
 import SelectInput, { SelectOptionType } from '../Shared/UI/Form/Select';
 import { Link } from 'react-router-dom';
 import LoadingButton from '../Shared/UI/LoadingButton';
+import CollapsibleCard from '../Shared/UI/CollapsibleCard';
+import DoubleClickButton from '../Shared/UI/DoubleClickButton';
+
+const MatchTitle: React.FC<{ match: DrawMatchEntity }> = ({ match }) => {
+  if (match.name) {
+    return <span>{match.name}</span>;
+  } else if (match.firstTeamId && match.secondTeamId) {
+    return <span>Teams</span>;
+  } else if (match.firstTeamPlaceholder && match.firstTeamPlaceholder) {
+    return (
+      <span>{`${match.firstTeamPlaceholder} x ${match.secondTeamPlaceholder}`}</span>
+    );
+  }
+  return <span>Match</span>;
+};
 
 interface MatchFormProps {
+  currentMatchValue: DrawMatchEntity;
   name: string;
   onRemove: () => {};
   selectInputTeams: SelectOptionType[];
 }
 
 const MatchForm: React.FC<MatchFormProps> = ({
+  currentMatchValue,
   name,
   onRemove,
   selectInputTeams
 }) => {
+  const DeleteButton = (
+    <DoubleClickButton className="button is-text" onClick={onRemove}>
+      <i className="fas fa-trash" />
+    </DoubleClickButton>
+  );
+
   return (
-    <div className="card">
-      <div className="card-content">
-        <div className="field">
-          <label className="label">First team</label>
+    <CollapsibleCard
+      titleElement={<MatchTitle match={currentMatchValue} />}
+      headerButtonsElement={DeleteButton}
+    >
+      <div className="field">
+        <label className="label">First team</label>
 
-          <div className="control">
-            <Field
-              name={`${name}.firstTeamId`}
-              render={(props: FieldRenderProps<string, HTMLSelectElement>) => (
-                <SelectInput {...props} options={selectInputTeams} />
-              )}
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">First parent match id</label>
-
-          <div className="control">
-            <Field
-              name={`${name}.firstTeamParentMatchId`}
-              component={StringInput}
-              type="text"
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">First placeholder</label>
-
-          <div className="control">
-            <Field
-              name={`${name}.firstTeamPlaceholder`}
-              component={StringInput}
-              type="text"
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">First team score</label>
-          <div className="control">
-            <Field
-              name={`${name}.firstTeamScore`}
-              component={StringInput}
-              type="number"
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Informations</label>
-
-          <div className="control">
-            <Field name={`${name}.info`} component={StringInput} type="text" />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Name</label>
-
-          <div className="control">
-            <Field name={`${name}.name`} component={StringInput} type="text" />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Second team</label>
-
-          <div className="control">
-            <Field
-              name={`${name}.secondTeamId`}
-              render={(props: FieldRenderProps<string, HTMLSelectElement>) => (
-                <SelectInput {...props} options={selectInputTeams} />
-              )}
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Second parent match id</label>
-
-          <div className="control">
-            <Field
-              name={`${name}.secondTeamParentMatchId`}
-              component={StringInput}
-              type="text"
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Second placeholder</label>
-
-          <div className="control">
-            <Field
-              name={`${name}.secondTeamPlaceholder`}
-              component={StringInput}
-              type="text"
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Second team score</label>
-          <div className="control">
-            <Field
-              name={`${name}.secondTeamScore`}
-              component={StringInput}
-              type="number"
-            />
-          </div>
+        <div className="control">
+          <Field
+            name={`${name}.firstTeamId`}
+            render={(props: FieldRenderProps<string, HTMLSelectElement>) => (
+              <SelectInput {...props} options={selectInputTeams} />
+            )}
+          />
         </div>
       </div>
 
-      <footer className="card-footer">
-        <div className="card-footer-item">
-          <button className="button is-warning" onClick={onRemove}>
-            Remove
-          </button>
+      <div className="field">
+        <label className="label">First parent match id</label>
+
+        <div className="control">
+          <Field
+            name={`${name}.firstTeamParentMatchId`}
+            component={StringInput}
+            type="text"
+          />
         </div>
-      </footer>
-    </div>
+      </div>
+
+      <div className="field">
+        <label className="label">First placeholder</label>
+
+        <div className="control">
+          <Field
+            name={`${name}.firstTeamPlaceholder`}
+            component={StringInput}
+            type="text"
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">First team score</label>
+        <div className="control">
+          <Field
+            name={`${name}.firstTeamScore`}
+            component={StringInput}
+            type="number"
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Informations</label>
+
+        <div className="control">
+          <Field name={`${name}.info`} component={StringInput} type="text" />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Name</label>
+
+        <div className="control">
+          <Field name={`${name}.name`} component={StringInput} type="text" />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Second team</label>
+
+        <div className="control">
+          <Field
+            name={`${name}.secondTeamId`}
+            render={(props: FieldRenderProps<string, HTMLSelectElement>) => (
+              <SelectInput {...props} options={selectInputTeams} />
+            )}
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Second parent match id</label>
+
+        <div className="control">
+          <Field
+            name={`${name}.secondTeamParentMatchId`}
+            component={StringInput}
+            type="text"
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Second placeholder</label>
+
+        <div className="control">
+          <Field
+            name={`${name}.secondTeamPlaceholder`}
+            component={StringInput}
+            type="text"
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Second team score</label>
+        <div className="control">
+          <Field
+            name={`${name}.secondTeamScore`}
+            component={StringInput}
+            type="number"
+          />
+        </div>
+      </div>
+    </CollapsibleCard>
   );
 };
 
@@ -155,11 +171,12 @@ interface FormProps extends FormRenderProps<DrawEntity> {
 const Form: React.FC<FormProps> = ({
   backUrl,
   isLoading,
+  selectInputTeams,
   handleSubmit,
   submitting,
   push,
   pristine,
-  selectInputTeams
+  values
 }) => {
   return (
     <div>
@@ -174,28 +191,32 @@ const Form: React.FC<FormProps> = ({
 
         <div className="field">
           <label className="label">Matches</label>
+
+          <FieldArray name="matches">
+            {({ fields }) =>
+              fields.map((name, index) => {
+                return (
+                  <MatchForm
+                    key={name}
+                    name={name}
+                    currentMatchValue={values.matches[index]}
+                    onRemove={() => fields.remove(index)}
+                    selectInputTeams={selectInputTeams}
+                  />
+                );
+              })
+            }
+          </FieldArray>
+
+          <button
+            className="button is-fullwidth is-medium"
+            type="button"
+            style={{ marginTop: '1.5rem' }}
+            onClick={() => push('matches', DEFAULT_DRAW_MATCH)}
+          >
+            Add match
+          </button>
         </div>
-
-        <FieldArray name="matches">
-          {({ fields }) =>
-            fields.map((name, index) => (
-              <MatchForm
-                key={name}
-                name={name}
-                onRemove={() => fields.remove(index)}
-                selectInputTeams={selectInputTeams}
-              />
-            ))
-          }
-        </FieldArray>
-
-        <button
-          className="button is-fullwidth"
-          type="button"
-          onClick={() => push('matches', DEFAULT_DRAW_MATCH)}
-        >
-          Add match
-        </button>
 
         <LoadingButton
           isLoading={isLoading}
