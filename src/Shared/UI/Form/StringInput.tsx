4937e7c78689e43e920bf3c07cf721dd9a7df5a5
Lair Júnior <lairjr@outlook.com>
2020-03-18 01:37:19 -0400

    Display errors and heightlight on string input

--- src/Shared/UI/Form/StringInput.tsx
+++ src/Shared/UI/Form/StringInput.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { FieldRenderProps } from 'react-final-form';
+import classNames from 'classnames';
+import './StringInput.scss';
 
 const StringInput: React.FC<FieldRenderProps<string, HTMLInputElement>> = ({
   input,
   meta
-}) => <input className="input" type="text" {...input} />;
+}) => {
+  const classes = classNames('input', {
+    'is-warning': !meta.dirtySinceLastSubmit && meta.invalid
+  });
+
+  return (
+    <Fragment>
+      <input className={classes} type="text" {...input} />
+
+      {meta.submitError &&
+        meta.submitError.map((error: string) => (
+          <p className="help is-warning">{error}</p>
+        ))}
+    </Fragment>
+  );
+};
 
 export default StringInput;
