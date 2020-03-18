782e6863d2e331935ac042021cca3caf93630947
Lair Júnior <lairjr@outlook.com>
2020-03-18 18:45:18 -0400

    Refactor meta logic in string input to its own component

---
+++ src/Shared/UI/Form/MetaInput.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React, { Fragment, ReactNode } from 'react';
+import { FieldMetaState } from 'react-final-form';
+import './MetaInput.scss';
+
+interface MetaInputProps {
+  component: (inputMetaClasses: string) => ReactNode;
+  meta: FieldMetaState<string>;
+}
+
+const MetaInput: React.FC<MetaInputProps> = ({ component, meta }) => {
+  const inputMetaClasses = classNames({
+    'is-warning': !meta.dirtySinceLastSubmit && meta.invalid
+  });
+
+  return (
+    <Fragment>
+      {component(inputMetaClasses)}
+
+      {meta.submitError &&
+        meta.submitError.map((error: string) => (
+          <p className="help is-warning">{error}</p>
+        ))}
+    </Fragment>
+  );
+};
+
+export default MetaInput;
--- src/Shared/UI/Form/StringInput.tsx
+++ src/Shared/UI/Form/StringInput.tsx
@@ -1,25 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { FieldRenderProps } from 'react-final-form';
-import classNames from 'classnames';
-import './StringInput.scss';
+import MetaInput from './MetaInput';
 
 const StringInput: React.FC<FieldRenderProps<string, HTMLInputElement>> = ({
   input,
   meta
 }) => {
-  const classes = classNames('input', {
-    'is-warning': !meta.dirtySinceLastSubmit && meta.invalid
-  });
-
   return (
-    <Fragment>
-      <input className={classes} type="text" {...input} />
-
-      {meta.submitError &&
-        meta.submitError.map((error: string) => (
-          <p className="help is-warning">{error}</p>
-        ))}
-    </Fragment>
+    <MetaInput
+      component={inputMetaClasses => (
+        <input className={`input ${inputMetaClasses}`} type="text" {...input} />
+      )}
+      meta={meta}
+    />
   );
 };
 
