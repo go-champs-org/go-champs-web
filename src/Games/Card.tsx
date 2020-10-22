4a3798419d6dfe0f78c3abfde040f42a75fb7b9b
Lair Júnior <lairjr@outlook.com>
2020-10-22 00:57:47 -0400

    Adjust game card mobile layout

--- src/Games/Card.tsx
+++ src/Games/Card.tsx
@@ -13,26 +13,57 @@ function Card({ game }: { game: GameEntity }): React.ReactElement {
           <small>{game.location}</small>
         </div>
 
-        <div className="column is-5 has-text-right">
-          <h2 className="title">{game.homeTeam.name}</h2>
-        </div>
-
-        <div className="column is-2 has-text-centered">
-          <div className="columns is-vcentered is-gapless">
-            <div className="column is-5 has-text-centered">
-              <span className="title">{game.homeScore}</span>
+        <div className="column is-12 is-hidden-mobile">
+          <div className="columns">
+            <div className="column is-5 has-text-right">
+              <h2 className="title">{game.homeTeam.name}</h2>
             </div>
 
-            <div className="column is-2">x</div>
+            <div className="column is-2 has-text-centered">
+              <div className="columns is-vcentered is-gapless">
+                <div className="column is-5 has-text-centered">
+                  <span className="title">{game.homeScore}</span>
+                </div>
 
-            <div className="column is-5 has-text-centered">
-              <span className="title">{game.awayScore}</span>
+                <div className="column is-2 has-text-centered">
+                  <span
+                    className="icon is-small"
+                    style={{ paddingTop: '.8rem' }}
+                  >
+                    <i className="fas fa-times"></i>
+                  </span>
+                </div>
+
+                <div className="column is-5 has-text-centered">
+                  <span className="title">{game.awayScore}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="column is-5 has-text-left">
+              <h2 className="title">{game.awayTeam.name}</h2>
             </div>
           </div>
         </div>
 
-        <div className="column is-5 has-text-left">
-          <h2 className="title">{game.awayTeam.name}</h2>
+        <div className="column is-12 is-hidden-tablet">
+          <div className="columns is-mobile is-multiline">
+            <div className="column is-8">
+              <span className="title is-size-5">{game.homeTeam.name}</span>
+            </div>
+
+            <div className="column is-4 has-text-right">
+              <span className="title is-size-5">{game.homeScore}</span>
+            </div>
+
+            <div className="column is-8">
+              <span className="title is-size-5">{game.awayTeam.name}</span>
+            </div>
+
+            <div className="column is-4 has-text-right">
+              <span className="title is-size-5">{game.awayScore}</span>
+            </div>
+          </div>
         </div>
 
         <div className="column is-12 has-text-centered">
