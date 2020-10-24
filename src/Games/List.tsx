d0372d52b973fe107e2d9905adc49c426bf997dc
Lair Júnior <lairjr@outlook.com>
2020-10-24 00:27:15 -0400

    Display date correctly in game list

--- src/Games/List.tsx
+++ src/Games/List.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { dateFromDate, timeFromDate } from '../Shared/datetime/format';
 import { GameEntity } from './state';
 import { Dispatch, AnyAction } from 'redux';
 import Shimmer from '../Shared/UI/Shimmer';
 import DoubleClickButton from '../Shared/UI/DoubleClickButton';
+import { Trans } from 'react-i18next';
 
 const LoadingCard: React.FC = () => (
   <div className="card item">
@@ -36,54 +36,57 @@ const GameCard: React.FC<{
   onDeleteGame: any;
   url: string;
   game: GameEntity;
-}> = ({ onDeleteGame, url, game }) => (
-  <div className="card item">
-    <div className="card-header">
-      <Link className="card-header-title" to={url}>
-        <div className="columns" style={{ flex: '1' }}>
-          <div className="column is-4 has-text-centered">
-            <span className="title is-6">
-              {game.awayTeam && game.awayTeam.name}
-            </span>
+}> = ({ onDeleteGame, url, game }) => {
+  return (
+    <div className="card item">
+      <div className="card-header">
+        <Link className="card-header-title" to={url}>
+          <div className="columns" style={{ flex: '1' }}>
+            <div className="column is-4 has-text-centered">
+              <span className="title is-6">
+                {game.awayTeam && game.awayTeam.name}
+              </span>
+            </div>
+            <div className="column is-2 has-text-centered">
+              <span className="title is-7">{game.awayScore}</span>
+            </div>
+            <div className="column is-2 has-text-centered">
+              <span className="title is-7">{game.homeScore}</span>
+            </div>
+            <div className="column is-4 has-text-centered">
+              <span className="title is-6">
+                {game.homeTeam && game.homeTeam.name}
+              </span>
+            </div>
           </div>
-          <div className="column is-2 has-text-centered">
-            <span className="title is-7">{game.awayScore}</span>
-          </div>
-          <div className="column is-2 has-text-centered">
-            <span className="title is-7">{game.homeScore}</span>
-          </div>
-          <div className="column is-4 has-text-centered">
-            <span className="title is-6">
-              {game.homeTeam && game.homeTeam.name}
-            </span>
-          </div>
+        </Link>
+        <div className="card-header-icon">
+          <DoubleClickButton
+            className="button is-text"
+            onClick={() => onDeleteGame(game)}
+          >
+            <i className="fas fa-trash" />
+          </DoubleClickButton>
         </div>
-      </Link>
-      <div className="card-header-icon">
-        <DoubleClickButton
-          className="button is-text"
-          onClick={() => onDeleteGame(game)}
-        >
-          <i className="fas fa-trash" />
-        </DoubleClickButton>
       </div>
-    </div>
 
-    <footer className="card-footer">
-      <div className="columns is-mobile" style={{ flex: '1' }}>
-        <div className="column is-6 has-text-centered">
-          <span className="title is-7">
-            {game.datetime &&
-              `${dateFromDate(game.datetime)} : ${timeFromDate(game.datetime)}`}
-          </span>
+      <footer className="card-footer">
+        <div className="columns is-mobile" style={{ flex: '1' }}>
+          <div className="column is-6 has-text-centered">
+            <span className="title is-7">
+              {game.datetime && (
+                <Trans values={{ date: game.datetime }}>date</Trans>
+              )}
+            </span>
+          </div>
+          <div className="column is-6 has-text-centered">
+            <span className="title is-7">{game.location}</span>
+          </div>
         </div>
-        <div className="column is-6 has-text-centered">
-          <span className="title is-7">{game.location}</span>
-        </div>
-      </div>
-    </footer>
-  </div>
-);
+      </footer>
+    </div>
+  );
+};
 
 const List: React.FC<{
   baseUrl: string;
