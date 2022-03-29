e343bb50f338bcb19136b04b914f34e231d2ac03
Lair Júnior <lairjr@outlook.com>
2022-03-29 02:02:58 -0400

    Add deploy staging step

--- env.staging.js
+++ env.staging.js
@@ -1,9 +1,8 @@
 module.exports = {
   "CI": false,
-  "NODE_OPTIONS": "--openssl-legacy-provider",
   "REACT_APP_ENV": "staging",
   "REACT_APP_FACEBOOK_APP_ID": process.env.FACEBOOK_APP_ID,
-  "REACT_APP_BUILD_NUMBER": process.env.TRAVIS_BUILD_NUMBER,
+  "REACT_APP_BUILD_NUMBER": process.env.GITHUB_RUN_NUMBER,
   "REACT_APP_API_HOST": process.env.STAGING_API_HOST,
   "REACT_APP_RECAPTCHA_SITE_KEY": process.env.STAGING_RECAPTCHA_SITE_KEY,
 };
