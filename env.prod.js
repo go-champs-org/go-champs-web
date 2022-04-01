7431ac78ad4b7fa7ee1268820e60b5fa9691f499
Lair Júnior <lairjr@outlook.com>
2022-04-01 18:24:10 -0400

    Add deploy admin/prod and admin e2e testing

--- env.prod.js
+++ env.prod.js
@@ -3,7 +3,7 @@ module.exports = {
   "NODE_OPTIONS": "--openssl-legacy-provider",
   "REACT_APP_ENV": "prod",
   "REACT_APP_FACEBOOK_APP_ID": process.env.FACEBOOK_APP_ID,
-  "REACT_APP_BUILD_NUMBER": process.env.TRAVIS_BUILD_NUMBER,
+  "REACT_APP_BUILD_NUMBER": process.env.GITHUB_RUN_NUMBER,
   "REACT_APP_API_HOST": process.env.PROD_API_HOST,
   "REACT_APP_RECAPTCHA_SITE_KEY": process.env.PROD_RECAPTCHA_SITE_KEY,
 };
