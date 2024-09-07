917a58f18c9853c44be9a17eadfacd43f9f100ff
Lair Junior <lairjr@outlook.com>
2024-09-07 10:37:29 -0400

    Add GA script (#49)
    * Add GA script
    
    * Bootstrap in typescript

--- src/Shared/analytics/analytics.ts
+++ src/Shared/analytics/analytics.ts
@@ -1,9 +1,16 @@
 import * as amplitude from '@amplitude/analytics-browser';
-import { REACT_APP_AMPLITUDE_API_KEY, REACT_APP_ENV } from '../env';
+import {
+  REACT_APP_AMPLITUDE_API_KEY,
+  REACT_APP_ENV,
+  REACT_APP_GA_ID
+} from '../env';
 
 const bootstrap = () => {
   if (REACT_APP_ENV === 'prod') {
     amplitude.init(REACT_APP_AMPLITUDE_API_KEY || '');
+    gtag('js', new Date());
+
+    gtag('config', REACT_APP_GA_ID || '');
   } else {
     console.log('Analytics disabled');
   }
