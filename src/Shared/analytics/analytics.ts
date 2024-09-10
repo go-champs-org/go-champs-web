01c3159c7198269fe0576a3b59efbae9f0f6e5c5
Lair Junior <lairjr@outlook.com>
2024-09-10 13:19:38 +0000

    Disable default tracking message

--- src/Shared/analytics/analytics.ts
+++ src/Shared/analytics/analytics.ts
@@ -7,7 +7,9 @@ import {
 
 const bootstrap = () => {
   if (REACT_APP_ENV === 'prod') {
-    amplitude.init(REACT_APP_AMPLITUDE_API_KEY || '');
+    amplitude.init(REACT_APP_AMPLITUDE_API_KEY || '', {
+      defaultTracking: true
+    });
     gtag('js', new Date());
 
     gtag('config', REACT_APP_GA_ID || '');
