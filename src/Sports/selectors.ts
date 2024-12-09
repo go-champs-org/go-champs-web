e31cfdaba84ce100b148966c459c55ea7af67af8
Lair Junior <lairjr@outlook.com>
2024-12-09 23:25:15 +0000

    Load sport statistics

--- src/Sports/selectors.ts
+++ src/Sports/selectors.ts
@@ -1,4 +1,4 @@
-import { SportState, SportEntity } from './state';
+import { SportState } from './state';
 
 export const sports = (state: SportState) =>
   Object.keys(state.sports).map((key: string) => state.sports[key]);
--- src/Sports/sportHttpClient.ts
+++ src/Sports/sportHttpClient.ts
@@ -1,7 +1,7 @@
 import { REACT_APP_API_HOST } from '../Shared/env';
 import {
-  ApiSportsResponse,
-  ApiSportResponse
+  ApiSportResponse,
+  ApiSportsResponse
 } from '../Shared/httpClient/apiTypes';
 import httpClient from '../Shared/httpClient/httpClient';
 import { mapApiSportToSportEntity } from './dataMappers';
@@ -9,11 +9,19 @@ import { SportEntity } from './state';
 
 const SPORTS_API = `${REACT_APP_API_HOST}v1/sports`;
 
+export const get = async (sportSlug: string): Promise<SportEntity> => {
+  const { data } = await httpClient.get<ApiSportResponse>(
+    `${SPORTS_API}/${sportSlug}`
+  );
+  return mapApiSportToSportEntity(data);
+};
+
 export const getAll = async (): Promise<SportEntity[]> => {
   const { data } = await httpClient.get<ApiSportsResponse>(SPORTS_API);
   return data.map(mapApiSportToSportEntity);
 };
 
 export default {
+  get,
   getAll
 };
