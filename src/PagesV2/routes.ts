27ee681545afe900ed64cd31de4706c5c97e8904
Lair Junior <lairjr@outlook.com>
2026-05-19 19:06:47 -0300

    Migrate organization view

--- src/PagesV2/routes.ts
+++ src/PagesV2/routes.ts
@@ -11,6 +11,34 @@ const V2_PATHS = new Set([
   '/TermsBR'
 ]);
 
+// First path segments reserved for non-organization routes
+const RESERVED_SEGMENTS = new Set([
+  'Organization',
+  'Account',
+  'FacebookSignUp',
+  'Invite',
+  'PrivacyPolicyBR',
+  'PrivacyPolicy',
+  'TermsBR',
+  'UseAsApp',
+  'SignIn',
+  'SignUp',
+  'AccountRecovery',
+  'AccountReset',
+  'Contact',
+  'About',
+  'Faq',
+  'Search'
+]);
+
 export function isLocationV2(pathname: string): boolean {
-  return V2_PATHS.has(pathname);
+  if (V2_PATHS.has(pathname)) return true;
+
+  // Single-segment paths not reserved for other routes are organization views (/:organizationSlug)
+  const segments = pathname.split('/').filter(Boolean);
+  if (segments.length === 1 && !RESERVED_SEGMENTS.has(segments[0])) {
+    return true;
+  }
+
+  return false;
 }
