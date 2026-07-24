eea49ae4df4ee9e51885b6aee680944002d48196
Ruan Victor <ruan.techno@gmail.com>
2026-07-23 22:42:28 -0300

    fix: skip API tournament mapper when registration tournament is absent
    Avoids feeding a camelCase TournamentEntity default into a mapper that
    expects snake_case API fields; tournament state already defaults to
    DEFAULT_TOURNAMENT.

--- src/Pages/TeamRosterInvites.tsx
+++ src/Pages/TeamRosterInvites.tsx
@@ -55,11 +55,13 @@ function TeamRosterInvites({ match }: TeamRosterInvitesProps) {
       setInvite(
         mapApiRegistrationInviteToRegistrationInviteEntity(invite.data)
       );
-      setTournament(
-        mapApiTournamentToTournamentEntity(
-          invite.data.registration.tournament || DEFAULT_TOURNAMENT
-        )
-      );
+      if (invite.data.registration.tournament) {
+        setTournament(
+          mapApiTournamentToTournamentEntity(
+            invite.data.registration.tournament
+          )
+        );
+      }
       setRegistration(
         mapApiRegistrationToRegistrationEntity(invite.data.registration)
       );
